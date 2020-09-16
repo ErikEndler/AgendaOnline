7021d7520251a2a8cc01bfa21dfdbf6d764d9fc2
Erik Endler <eng.erik.endler@gmail.com>
2020-09-16 19:20:20 -0300

    ajuste mascara

--- src/app/pages/usuario/usuario.component.ts
+++ src/app/pages/usuario/usuario.component.ts
@@ -5,6 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './usuario.component.html',
 })
 export class UsuarioComponent implements OnInit {
+  maskFone = '000.000.000-00';
+  maskCpf = '000.000.000-00';
+
   constructor() {}
 
   ngOnInit(): void {}
