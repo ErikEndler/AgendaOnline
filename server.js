bc023cef2ed9ad131a5daca1427dfb377af6da4a
Erik Endler <eng.erik.endler@gmail.com>
2021-03-23 15:57:13 -0300

    ajueste

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.get('/*', (req, res) => {
 res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4201);
